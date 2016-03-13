822e14b00259b426dc4b3c7b79d6b55cebaf84f1
Stefan Djokic <djokics@elfak.rs>
2016-03-13 01:13:09 +0100

    Add 'add to new line' and 'remove from new line' functions

--- client/templates/admin/admin.js
+++ client/templates/admin/admin.js
@@ -1,11 +1,11 @@
 Template.admin.onCreated(function() {
   Session.setDefault('newStation',{})
-  Session.setDefault('newline',{})
+  Session.setDefault('newLine',[])
   Session.setDefault('selectedStation',{})
   Session.setDefault('selectedLine',{})
 
   Session.set('newStation',{})
-  Session.set('newline',{})
+  Session.set('newLine',[])
   Session.set('selectedStation',{})
   Session.set('selectedLine',{})
 });
--- client/templates/admin/selectedStation.js
+++ client/templates/admin/selectedStation.js
@@ -1,6 +1,13 @@
 Template.selectedStation.helpers({
   selectedStation: function() {
     return Session.get('selectedStation');
+  },
+  isInNewLine: function() {
+    const newLine = Session.get('newLine');
+    const selectedStation = Session.get('selectedStation');
+    return newLine.find(station => {
+      return station._id == selectedStation._id
+    })
   }
 })
 
@@ -13,5 +20,16 @@ Template.selectedStation.events({
   },
   'click .selected-station-delete:not(.disabled)': function() {
     Stations.remove(Session.get('selectedStation')._id);
+  },
+  'click .selected-station-add-to-line:not(.disabled)':function() {
+    let newLine = Session.get('newLine');
+    newLine.push(Session.get('selectedStation'));
+    Session.set('newLine', newLine);
+  },
+  'click .selected-station-remove-from-line:not(.disabled)':function() {
+    let newLine = Session.get('newLine').filter(station => {
+      return station._id != Session.get('selectedStation')._id
+    });
+    Session.set('newLine', newLine);
   }
 });
